0596b6a072c6e3447a8242a8c517553b7aaea3bb
Smit Mehta <mehta.smit92@gmail.com>
2019-03-31 20:24:25 -0400

    Update model.R

--- west-nile-virus/code/model.R
+++ west-nile-virus/code/model.R
@@ -3,6 +3,10 @@ library(ggplot2)
 library(ggmap)
 library(dplyr)
 library(splitstackshape)
+library(e1071)
+library(C50)
+library(randomForest)
+library(data.table)
 
 setwd("C:/Users/mehta/Documents/GitHub/kaggle-projects/west-nile-virus/data")
 
@@ -16,7 +20,40 @@ mapdata <- readRDS("mapdata_copyright_openstreetmap_contributors.rds")
 
 #converting variables to required format
 weather$Date <- as.Date(as.character(weather$Date), format = "%Y-%m-%d")
+train$Date <- as.Date(as.character(train$Date), format = "%Y-%m-%d")
+test$Date <- as.Date(as.character(test$Date), format = "%Y-%m-%d")
 
+
+
+
+
+
+weatherdata <- weather %>% group_by(Date) %>% summarise(AvgTemp = mean(Tmax, na.rm = TRUE))
+
+
+
+training <- merge(train, weatherdata, by = "Date", all.x = TRUE)
+testing <- merge(test, weatherdata, by = "Date", all.x = TRUE)
+
+training$WnvPresent <- as.factor(training$WnvPresent)
+
+
+t_Species<-c(as.character(training$Species),as.character(testing$Species))
+t_Species[t_Species=="UNSPECIFIED CULEX"]<-"CULEX ERRATICUS"
+t_Species<-factor(t_Species,levels=unique(t_Species))
+
+## data.table syntax for adding a column; could overwrite the existing column as well
+training <- as.data.table(training)[,Species2:=factor(t_Species[1:nrow(training)],levels=unique(t_Species))]
+testing <- as.data.table(testing)[,Species2:=factor(t_Species[(nrow(training)+1):length(t_Species)],levels=unique(t_Species))]
+
+
+
+#Naive Bayes Classification
+nb_model <- naiveBayes(WnvPresent ~ Species + Trap + AvgTemp, data = training)
+pred_nb_model <- predict(nb_model, newdata = testing)
+
+rf_model <- randomForest(WnvPresent ~ Species + AvgTemp, data = training, ntree = 200, importance = TRUE)
+pred_rf_model <- predict(rf_model, newdata = testing)
 
 
 
